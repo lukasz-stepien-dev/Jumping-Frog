ec3af64bc5dd5295ab94b8f9d74347fbd8b22a5d
lukasz-stepien-dev <lukaszatepien@gmail.com>
2024-12-15 20:03:03 +0100

    Simple connection from client to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Instruction from "./component/Instruction/Instruction";
 import Frog from "./component/Frog/Frog";
+import socketIO from 'socket.io-client';
+const socket = socketIO.connect('http://localhost:4000');
 
 export default function App() {
   return (
