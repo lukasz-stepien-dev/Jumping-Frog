3e33b68daf4bc2c46191b45dd8fed1ca6077c629
Łukasz Stępień <lukaszatepien@gmail.com>
2024-12-26 14:14:40 +0100

    Room logic

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
-//New imports
+// New imports
 const http = require('http').Server(app);
 const cors = require('cors');
 const socketIO = require('socket.io')(http, {
@@ -15,14 +15,21 @@ app.use(cors());
 
 socketIO.on('connection', (socket) => {
     console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('create', () => {
+        console.log(`🏠: ${socket.id} created room ${socket.id}`);
+        socket.join(socket.id);
+    });
+    socket.on('join', (room) => {
+        console.log(`🚪: ${socket.id} joined room ${room}`);
+        socket.join(room);
+    });
+    socket.on('jump', (room) => {
+        console.log(`🚪: ${socket.id} jumped to room ${room}`);
+        socket.join(room);
+        socketIO.in(room).emit('jump');
+    });
     socket.on('disconnect', () => {
         console.log('🔥: A user disconnected');
-    });
-});
-
-app.get('/api', (req, res) => {
-    res.json({
-        message: 'Hello world',
     });
 });
 
